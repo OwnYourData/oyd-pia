939f164ffafedb1975519259697e02519fcbdd92
michael <michael@appbakery.at>
2016-03-08 22:03:16 +0100

    install/start/stop plugin working

---
+++ src/main/webapp/scripts/app/entities/plugin/plugin-dialog.controller.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('piaApp').controller('PluginDialogController',
+    ['$scope', '$stateParams', '$uibModalInstance', 'entity', 'Plugin',
+        function($scope, $stateParams, $uibModalInstance, entity, Plugin) {
+
+        $scope.plugin = entity;
+        $scope.load = function(id) {
+            Plugin.get({id : id}, function(result) {
+                $scope.plugin = result;
+            });
+        };
+
+        var onSaveSuccess = function (result) {
+            $scope.$emit('piaApp:pluginUpdate', result);
+            $uibModalInstance.close(result);
+            $scope.isSaving = false;
+        };
+
+        var onSaveError = function (result) {
+            $scope.isSaving = false;
+        };
+
+        $scope.save = function () {
+            $scope.isSaving = true;
+            if ($scope.plugin.id != null) {
+                Plugin.update($scope.plugin, onSaveSuccess, onSaveError);
+            } else {
+                Plugin.save($scope.plugin, onSaveSuccess, onSaveError);
+            }
+        };
+
+        $scope.clear = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
+}]);
