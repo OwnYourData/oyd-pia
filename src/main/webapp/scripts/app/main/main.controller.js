3ef8373abfbe6d424c214298c6d9347ad11edc9d
michael <michael@appbakery.at>
2016-05-06 21:09:56 +0200

    added HerokuDatabaseConfiguration, removed login menu, added login on main screen

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,32 @@
 'use strict';
 
 angular.module('piaApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($rootScope, $scope, Principal,$state, $timeout, Auth) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+
+        $scope.user = {};
+        $scope.errors = {};
+
+        $scope.rememberMe = true;
+        $timeout(function (){angular.element('[ng-model="username"]').focus();});
+        $scope.login = function (event) {
+            event.preventDefault();
+            Auth.login({
+                username: $scope.username,
+                password: $scope.password,
+                rememberMe: $scope.rememberMe
+            }).then(function () {
+                $scope.authenticationError = false;
+                if ($rootScope.previousStateName === 'register') {
+                    $state.go('home');
+                } else {
+                    $rootScope.back();
+                }
+            }).catch(function () {
+                $scope.authenticationError = true;
+            });
+        };
     });
