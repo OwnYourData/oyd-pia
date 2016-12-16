e54da3cb0383a0e648f194f2a17f742b9215547c
michael <proesti@gmail.com>
2016-12-16 10:22:25 +0100

    trying to fix postgres groupBy SQL

--- src/main/java/eu/ownyourdata/pia/repository/RepoRepository.java
+++ src/main/java/eu/ownyourdata/pia/repository/RepoRepository.java
@@ -16,9 +16,9 @@ import java.util.Optional;
 public interface RepoRepository extends JpaRepository<Repo,Long> {
     Optional<Repo> findOneByIdentifier(String identifier);
 
-    @Query("Select new eu.ownyourdata.pia.domain.RepoItemCount(repo.id, repo.description, repo.identifier, count(items.id)) from Repo repo left join repo.items as items where repo.identifier like :wildcard% group by repo.identifier")
+    @Query("Select new eu.ownyourdata.pia.domain.RepoItemCount(repo.id, repo.description, repo.identifier, count(items.id)) from Repo repo left join repo.items as items where repo.identifier like :wildcard% group by repo.id")
     Collection<RepoItemCount> getRepoItemCounts(@Param("wildcard") String wildcard);
 
-    @Query("Select new eu.ownyourdata.pia.domain.RepoItemCount(repo.id, repo.description, repo.identifier, count(items.id)) from Repo repo left join repo.items as items group by repo.identifier")
+    @Query("Select new eu.ownyourdata.pia.domain.RepoItemCount(repo.id, repo.description, repo.identifier, count(items.id)) from Repo repo left join repo.items as items group by repo.id")
     Collection<RepoItemCount> getRepoItemCounts();
 }
