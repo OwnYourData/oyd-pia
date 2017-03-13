34f35356c4727a2e7ece95ae3d9bc14d033d8494
Christoph Fabianek <christoph.fabianek@gmail.com>
2017-03-13 23:08:42 +0100

    allow CORS

--- docker/oyd-pia/script/run.sh
+++ docker/oyd-pia/script/run.sh
@@ -3,17 +3,6 @@
 # Stop on error
 set -e
 
-# install gems
-# loop according to http://www.zhuwu.me/blog/posts/solve-gem-installation-timeout-when-building-docker-image
-N=0
-until [ ${N} -ge 5 ]
-do
-  cd /service-scheduler && bundle install  && break
-  echo 'Try bundle again ...'
-  N=$[${N}+1]
-  sleep 1
-done
-
 #start postgreSQL
 echo "starting postgreSQL ... "
 su postgres -c "postgres -D /var/lib/postgresql/data &"
